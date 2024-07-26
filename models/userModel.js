108bc9df226fc760d0f27e1e85ddeb21c44e4c0d
VSNSRUJANA123 <srujanadvsn@gmail.com>
2024-07-26 19:50:31 +0530

    update

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  assessments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Assessment" }],
 });
 
 userSchema.pre("save", async function (next) {
