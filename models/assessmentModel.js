186a4234ed56ad8f5fa14d3086d43f46b87fc0cf
VSNSRUJANA123 <srujanadvsn@gmail.com>
2024-07-26 19:25:41 +0530

    update

--- models/assessmentModel.js
+++ models/assessmentModel.js
@@ -4,7 +4,6 @@ const assessmentSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   dueDate: { type: Date, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 module.exports = mongoose.model("Assessment", assessmentSchema);
